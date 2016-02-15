18edefd05871949fd3403058101df9230bffb3c7
AidanPride <a.aidan@yandex.ru>
2016-02-15 18:25:56 +0200

    BT7_TIGER
    2 new tanks

---
+++ BattleTank_ver03/BT7.java
@@ -0,0 +1,8 @@
+
+public class BT7 extends Tank {
+private int speed = 2;
+
+    public BT7(ActionField af, BattleField bf, int x, int y, Direction direction) {
+        super(af, bf, x, y, direction);
+    }
+}
---
+++ BattleTank_ver03/Tiger.java
@@ -0,0 +1,8 @@
+
+public class Tiger extends Tank {
+    private int armor = 1;
+    private int speed = 7;
+    public Tiger(ActionField af, BattleField bf, int x, int y, Direction direction) {
+        super(af, bf, x, y, direction);
+    }
+}
